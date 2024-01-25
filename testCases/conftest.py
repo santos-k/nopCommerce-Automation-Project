b16081277b957c711a3a397c26e700d5d81bf1ae
Santosh Kumar <withusanty@gmail.com>
2024-01-25 16:01:22 +0530

    HTML Report Generate implemented

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 import pytest
-
+from pytest_metadata.plugin import metadata_key
 
 @pytest.fixture()
 def setup(browser):
@@ -19,10 +19,27 @@ def setup(browser):
     driver.close()
 
 
-def pytest_addoption(parser): # this will get the value from the CLI/Hooks
+def pytest_addoption(parser):  # this will get the value from the CLI/Hooks
     parser.addoption("--browser")
 
 
 @pytest.fixture()
-def browser(request): # this will return the browser value to the setup method
+def browser(request):  # this will return the browser value to the setup method
     return request.config.getoption("--browser")
+
+
+################ Pytest HTML Reports ########################
+# It is hook for Adding Environment info to HTML Report
+def pytest_configure(config):
+    metadata = config.pluginmanager.getplugin("metadata")
+    if metadata:
+        config.stash[metadata_key]['Project Name'] = 'nop Commerce'
+        config.stash[metadata_key]['Module Name'] = 'Login Page'
+        config.stash[metadata_key]['Tester'] = 'Santosh Kumar'
+
+
+# It is hook for delete/Modify Environment info to HTML Report
+@pytest.hookimpl(optionalhook=True)
+def pytest_metadata(metadata):
+    metadata.pop("JAVA_HOME", None)
+    metadata.pop("Plugins", None)
