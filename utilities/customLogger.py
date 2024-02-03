20e8e4fe3a80a0dc915a2f317db62af02294701e
Santosh Kumar <40932902+santos-k@users.noreply.github.com>
2024-02-03 11:36:14 +0530

    Duplicate logs entiries fixed

--- utilities/customLogger.py
+++ utilities/customLogger.py
@@ -4,6 +4,7 @@ class LogGen:
     @staticmethod
     def loggen():
         logger = logging.getLogger()
+        logger.handlers.clear()
         f_handler = logging.FileHandler(filename='.\\Logs\\automation.log', mode='a')
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
         f_handler.setFormatter(formatter)
